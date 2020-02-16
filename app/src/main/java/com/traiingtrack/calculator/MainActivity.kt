efc5f907b43b4466b5d4d475f90e350f175a834e
Derfel Cadarn <gehigbie@gmail.com>
2020-02-16 12:48:33 -0700

    Identify and use buttons

--- app/src/main/java/com/traiingtrack/calculator/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/calculator/MainActivity.kt
@@ -2,11 +2,53 @@ package com.traiingtrack.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var result: EditText
+    private lateinit var newNumber: EditText
+    private val displayOperation by lazy {findViewById<TextView>(R.id.operation)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        result = findViewById(R.id.result)
+        newNumber = findViewById(R.id.newNumber)
+
+        //data input buttons
+        val button0: Button = findViewById(R.id.button0)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+        val button5: Button = findViewById(R.id.button5)
+        val button6: Button = findViewById(R.id.button6)
+        val button7: Button = findViewById(R.id.button7)
+        val button8: Button = findViewById(R.id.button8)
+        val button9: Button = findViewById(R.id.button9)
+        val buttonDot: Button = findViewById(R.id.buttonDot)
+
+        //operation buttons
+        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
+        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
+        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
+        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+
+        val listener = View.OnClickListener { v ->
+            val b = v as Button
+            newNumber.append(b.text)
+        }
+
+        val dataButtons: ArrayList<Button> = arrayListOf(button0, button1, button2, button3,
+            button4, button5, button6, button7, button8, button9, buttonDot)
+
+        for(button in dataButtons) {
+            button.setOnClickListener(listener)
+        }
+
     }
 }
